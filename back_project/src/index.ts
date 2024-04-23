e8d0c0a382bd72daddb0d75def08aabcbae58a6d
Matheus Nazaro <85762668+mnazaro@users.noreply.github.com>
2024-04-23 20:11:29 -0300

    Update index.ts

--- back_project/src/index.ts
+++ back_project/src/index.ts
@@ -1,20 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 import empresaRoutes from './routes/EmpresaRoute';
 import setorRoutes from './routes/SetorRoute';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use('/empresa', empresaRoutes);
 app.use('/setor', setorRoutes);
-
-// Middleware para tratamento de erros
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
 app.listen(5000, '0.0.0.0', () => {
     console.log('Server is running on port 5000');
